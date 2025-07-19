e773d7e3fd899543099a900c6757c4995efd97fb
Sumith Kumar Saini <sumithsaini01@gmail.com>
2025-07-19 17:52:26 +0530

    feat: add custom fonts and update styles for Home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div className="w-full h-screen bg-zinc-900 flex justify-center items-center text-9xl uppercase text-stone-100">
+    <div className="w-full h-screen font-grotesk bg-zinc-900 flex justify-center items-center text-9xl uppercase text-stone-100">
       Home
     </div>
   );
