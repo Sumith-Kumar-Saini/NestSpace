ef3dbe5be9543cfd21e1f2a34b1d35236611358b
Sumith Kumar Saini <sumithsaini01@gmail.com>
2025-07-23 17:27:37 +0530

    refactor: update Jest configuration and add test script
    - Simplified Jest configuration by removing globals and adding a transform property for TypeScript files.
    - Added a test script to package.json to facilitate running tests with Jest.
    - Enabled isolatedModules in tsconfig.json to ensure safe transpilation of individual files.

--- server/jest.config.js
+++ server/jest.config.js
@@ -3,9 +3,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/tests/**/*.test.ts"],
-  globals: {
-    "ts-jest": {
-      isolatedModules: true,
-    },
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest"],
   },
 };
